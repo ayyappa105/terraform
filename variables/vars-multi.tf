f195fdf950961a3fbfa46f07ed121b811eab641a
ayyappa105 <vasireddyayyappa@gmail.com>
2024-04-14 19:19:31 +0530

    terraform

--- variables/vars-multi.tf
+++ variables/vars-multi.tf
@@ -7,5 +7,5 @@ variable "multi" {
 }
 
 output "multi_var" {
-  value = "Now a days for provisiong the ${0} ${1} tecnology used to achive ${2}"
+  value = "Now a days for provisiong the ${var.multi[0]} ${var.multi[1]} tecnology used to achive ${var.multi[2]}"
 }
